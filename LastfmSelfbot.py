0adb67ac9ead481c786af85df9ed6ffab0787c53
djpadbit <djpadbit@gmail.com>
2017-11-30 23:00:36 +0100

    Removed threading and made the corroutine run in discord.py's loop instead and a bunch of fixes
    Signed-off-by: djpadbit <djpadbit@gmail.com>

--- LastfmSelfbot.py
+++ LastfmSelfbot.py
@@ -1,4 +1,3 @@
-import os
 #libraries related to last.fm
 try:
 	from urllib.request import urlopen
@@ -7,7 +6,6 @@ except ImportError:
 	from urllib2 import urlopen
 from xml.dom import minidom
 import sys
-import time
 import argparse
 import codecs
 import threading
@@ -39,7 +37,7 @@ bot = discord.Client()
 local_copy = 'nowplaying.xml'
 
 def get_parameters():
-	parser = argparse.ArgumentParser(prog='NowPlayingToTxt',description="description")
+	parser = argparse.ArgumentParser(prog='LastfmSelfbot',description="description")
 	parser.add_argument('username')
 	parser.add_argument('-p', '--prepend', dest = 'prepend', default = defaults['prepend'])
 	parser.add_argument('-a', '--append', dest = 'append', default = defaults['append'])
@@ -78,13 +76,14 @@ async def on_ready():
 	print('Line them up, knock em down! last.fm bot ready for action.')
 	await bot.change_presence(status=discord.Status.invisible)
 	print('--------')
-	threading.Thread(target=lastfm_thread).start()
+	asyncio.run_coroutine_threadsafe(lastfm_coro(),bot.loop)
+	#Launch the cororoutine in discord.py's event loop. It's probably a lot safer than the jenky ass thread way
 
 enabled = True #set to True if you want the last.fm script to start at boot, set to false if you want to manually start the script
+alive = True # Coro's life
 
-async def lastfm_thread_async():
-	global enabled
-	get_parameters()
+async def lastfm_coro():
+	global enabled,alive
 	print('Starting with arguments:')
 	print('Username: ' + args['username'])
 	print('Prepended text: ' + args['prepend'])
@@ -99,7 +98,7 @@ async def lastfm_thread_async():
 	feed_url = ('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user='
 		+ args['username'] + '&api_key=' + api_key + '&limit=1')
 
-	while True:
+	while alive:
 		if enabled:
 			download(feed_url,local_copy)
 			
@@ -134,40 +133,34 @@ async def lastfm_thread_async():
 					last_track = ''
 					track_data = ''
 					await bot.change_presence()
-			#Need to wait at least 1 second for another API call
-			time.sleep(args['delay'])
-
-def lastfm_thread():
-	loop = asyncio.new_event_loop()
-	asyncio.set_event_loop(loop)
-	loop.run_until_complete(lastfm_thread_async())
+		#Need to wait at least 1 second for another API call
+		await asyncio.sleep(args['delay'])
 
 #actual bot stuff happens under here	
 	
 @bot.event
 async def on_message(message):
-	global enabled
+	global enabled,alive
 	if message.server.id in whitelist: #check in the whitelist list if the server ID is in it
 		if message.author == bot.user: #checks if the message is from the user
-
 			if message.content.startswith(prefix + ' on'):
-				if enabled == False: #is it already enabled?
+				if not enabled: #is it already enabled?
 					enabled = True
 					print("Enabled")
 					await bot.change_presence(status=discord.Status.online)
 					await bot.delete_message(message)
-				elif enabled == True: 
+				else: 
 					await bot.edit_message(message, new_content="last.fm bot is already enabled!")
 					await asyncio.sleep(1)
 					await bot.delete_message(message)
 
 			elif message.content.startswith(prefix + ' off'):
-				if enabled == True:
+				if enabled:
 					enabled = False
 					await bot.change_presence(status=discord.Status.invisible)
 					print("Disabled")
 					await bot.delete_message(message)
-				elif enabled == False:
+				else:
 					await bot.edit_message(message, new_content="last.fm bot is already disabled!")
 					await asyncio.sleep(1)
 					await bot.delete_message(message)
@@ -175,8 +168,11 @@ async def on_message(message):
 			elif message.content.startswith(prefix + ' shutdown'):
 				await bot.delete_message(message)
 				await bot.change_presence(status=discord.Status.invisible)
-				os._exit(0)
-		
+				alive = False
+				await asyncio.sleep(args['delay']*2)
+				await bot.logout()
+				await bot.close()
 
 if __name__=="__main__":
+	get_parameters()
 	bot.run(TOKEN, bot=False)
